d4d47a55023cb4e617e94c9eaedbf53a3f559410
Benjamin Sinkula <bsinky@gmail.com>
2018-04-28 09:16:16 -0500

    Initial mostly empty commit

---
+++ README.md
@@ -0,0 +1,4 @@
+hx0b
+====
+
+Adds binary numeric literals to Haxe with the power of macros.
---
+++ hx0b.hx
@@ -0,0 +1,13 @@
+#if macro
+import haxe.macro.Context;
+import haxe.macro.Expr;
+using tink.MacroApi;
+#end
+
+class Hx0b
+{
+	macro public static function binary(arr:ExprOf<String>)
+	{
+
+	}
+}
